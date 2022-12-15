a3e89703dc52afb80d7b339aa2613cc2a7fd505d
notiona <notiona@snu.ac.kr>
2022-12-16 00:45:49 +0900

    update visualizer help message

--- datapilot/constants/help_message.py
+++ datapilot/constants/help_message.py
@@ -32,6 +32,16 @@ VISUALIZER_HELP_MESSAGE = \
 """
 help():
     List all functions of Visualizer
+get_all_column_info():
+    Get column name and type of self.df
+visualize_one_column():
+    Column-wise quick visualization
+visualize_two_columns():
+    Inter column quick visualization, depending on input type
+visualize_pca_2d():
+    Low-code PCA + 2d visualization
+visualize_pca_3d():
+    Low-code PCA + 3d visualization
 """
 
 VISUALIZER_TWO_COLUMNS_HELP_MESSAGE = \
